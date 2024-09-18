7656f0ecb5d3a10c3654f80baf7b4b1d9eb9f840
MrROBUST <mrrobust@yandex.ru>
2024-09-18 22:32:47 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    auto w = new MainWindow(); // deletes itself on close
+    w->show();
+    return a.exec();
+}
